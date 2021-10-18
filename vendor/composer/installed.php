3eaa5d4b90b21623b4eac1c9672c121b0156ca49
Amandine Lalle <amandinelalle51@gmail.com>
2021-10-18 12:10:23 +0200

    quete done

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1648b6567e2747de05c516293b70c05d9e280f21',
+        'name' => 'wilder/composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wilder/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1648b6567e2747de05c516293b70c05d9e280f21',
+            'dev_requirement' => false,
+        ),
+    ),
+);
